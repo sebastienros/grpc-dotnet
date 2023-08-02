cda97c7b189ead8944e804e55efd897bcd42823c
James Newton-King <james@newtonking.com>
2023-08-02 09:46:43 +0800

    Update InteropClient.cs

--- testassets/Shared/InteropClient.cs
+++ testassets/Shared/InteropClient.cs
@@ -867,7 +867,7 @@ public class InteropClient
         string keyFile = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS")!;
         Assert.IsNotNull(keyFile);
         var jobject = JObject.Parse(File.ReadAllText(keyFile));
-        string email = jobject.GetValue("client_email")!.Value<string>();
+        string email = jobject.GetValue("client_email")!.Value<string>()!;
         Assert.IsTrue(email.Length > 0);  // spec requires nonempty client email.
         return email;
     }
